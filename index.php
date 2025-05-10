afabfc5dbe2df3e727dff7fd29b22ead1216ab90
AbdoulayeAbdoulaye Alhassane Diallo <alhassaneabdoulaye@gmail.com>
2025-05-10 09:05:56 -0400

    done

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ if ($status === 'success') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Computer Science Project Grading</title>
-    <link rel="stylesheet" href="/studentSuccessGrade/style.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <div class="container">
